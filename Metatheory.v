2b0450afa8cbeaa8f920cc1184f5af7e8a29992b
Mario Alvarez <mario.alvarez739@gmail.com>
2015-01-23 16:45:34 +0100

    DID IT

--- Metatheory.v
+++ Metatheory.v
@@ -92,7 +92,6 @@ Lemma shift_var_s : forall (v i : nat), shift_var (S v) (S i) = S (shift_var v i
 Qed.
 
 Lemma insert_kind_get_kind_lt : forall (e e' : env), forall (i v : nat),
-                                  i <= v ->
                                   insert_kind_r i v e e' ->
                                   forall (w : nat),
                                     w < i ->
@@ -103,23 +102,22 @@ Lemma insert_kind_get_kind_lt : forall (e e' : env), forall (i v : nat),
   induction e.
   - intros. simpl. trivial.
   - intros. simpl. destruct e'.
-    + inversion H0.
-    + simpl. apply (IHe e' i v). simpl in H0. apply H. apply H0. apply H1.
-    + inversion H0.
+    + inversion H.
+    + simpl. apply (IHe e' i v). simpl in H. apply H. apply H0.
+    + inversion H.
   - intros. destruct e'. 
-    + inversion H0.
-    + inversion H0.
+    + inversion H.
+    + inversion H.
     + destruct w.
       * destruct i.
         { omega. }
-        { destruct H0. rewrite H0. simpl. trivial. }
+        { destruct H. rewrite H. simpl. trivial. }
       * destruct i.
         { omega. }
-        { simpl. apply (IHe e' i v). omega. simpl in H0. apply H0. omega. }
+        { simpl. apply (IHe e' i v). apply H. omega. }
 Qed.
 
 Lemma insert_kind_get_kind_ge : forall (e e' : env), forall (i v : nat),
-                                  i <= v ->
                                   insert_kind_r i v e e' ->
                                   forall (w : nat),
                                     w >= i ->
@@ -130,66 +128,258 @@ Lemma insert_kind_get_kind_ge : forall (e e' : env), forall (i v : nat),
   induction e.
   - intros. simpl. trivial.
   - intros. simpl. destruct e'.
-    + inversion H0.
-    + simpl. apply (IHe e' i v). simpl in H0. apply H. apply H0. apply H1.
-    + inversion H0.
+    + inversion H.
+    + simpl. apply (IHe e' i v). simpl in H. apply H. apply H0.
+    + inversion H.
   - intros. destruct e'. 
-    + inversion H0.
-    + inversion H0.
+    + inversion H.
+    + inversion H.
     + destruct w.
       * destruct i.
-        { simpl in H0. rewrite H0. simpl. destruct (get_kind e' 0). simpl. trivial. trivial. }
+        { simpl in H. rewrite H. simpl. destruct (get_kind e' 0). simpl. trivial. trivial. }
         { omega. }
       * destruct i.
-        { simpl in H0. rewrite H0. simpl. destruct (get_kind e' (S w)). trivial. trivial. }
-        { simpl. apply (IHe e' i v). omega. simpl in H0. apply H0. omega. }
+        { simpl in H. rewrite H. simpl. destruct (get_kind e' (S w)). trivial. trivial. }
+        { simpl. apply (IHe e' i v). apply H. omega. }
 Qed.
         
-Lemma insert_kind_get_kind : forall (e e' : env), forall (v : nat),
-                               insert_kind v e e' ->
+Lemma insert_kind_get_kind : forall (e e' : env), forall (i v : nat),
+                               insert_kind_r i v e e' ->
                                forall (w : nat),
                                  match get_kind e w with
-                                   | Some k => get_kind e' (shift_var w v) = Some k
+                                   | Some k => get_kind e' (shift_var w i) = Some k
                                    | None => True
                                  end.
   intros.
   unfold shift_var.
-  destruct (le_gt_dec v w).
-  - apply (insert_kind_get_kind_ge e e' v v). trivial. apply H. omega.
-  - apply (insert_kind_get_kind_lt e e' v v). trivial. apply H. omega.
+  destruct (le_gt_dec i w).
+  - apply (insert_kind_get_kind_ge e e' i v H w l).
+  - apply (insert_kind_get_kind_lt e e' i v H w g).
 Qed.
 
-Lemma insert_kind_wf_typ : forall (t : typ), forall (e e' : env), forall (v : nat),
-                             insert_kind v e e' -> wf_typ e t -> wf_typ e' (tshift v t).
+Definition exists_kind (e : env) (n : nat) := match get_kind e n with Some _ => True | None => False end.
+Hint Transparent exists_kind : core.
+Hint Unfold exists_kind : core.
+
+Fixpoint highest_free_typ_variable (t : typ) :=
+  match t with
+    | tvar n => Some n
+    | tarr t1 t2 => match highest_free_typ_variable t1 with
+                      | None => highest_free_typ_variable t2
+                      | Some h1 => match highest_free_typ_variable t2 with
+                                     | None => Some h1
+                                     | Some h2 => if le_gt_dec h1 h2 then Some h2 else Some h1
+                                   end
+                    end
+    | tall _ t1 => match highest_free_typ_variable t1 with
+                     | None => None
+                     | Some 0 => None
+                     | Some (S n) => Some n
+                   end
+  end.
+
+Fixpoint num_typ_variables (e : env) :=
+  match e with
+    | empty => 0
+    | etvar _ e' => S (num_typ_variables e')
+    | evar _ e => num_typ_variables e
+  end.
+
+Lemma get_free_variable : forall e, forall n,
+                            n < num_typ_variables e <-> exists_kind e n.
+  induction e.
+  - simpl. intros. split. omega. intro. inversion H.
+  - intros. split.
+    + intro. simpl in H |- *. apply IHe. apply H.
+    + unfold exists_kind. simpl. apply IHe.
+  - intros. split.
+    + intro. unfold exists_kind.  simpl in H.
+      destruct n.
+      * simpl. trivial.
+      * apply IHe. auto with arith.
+    + intro. destruct n.
+      * simpl. auto with arith.
+      * simpl in H |- *. apply lt_n_S. apply IHe. apply H.
+Qed.
+
+Lemma num_typ_variables_significant : forall (t : typ), forall (e : env),
+                                        wf_typ e t <->
+                                        match highest_free_typ_variable t with
+                                          | None => True
+                                          | Some i => i < num_typ_variables e
+                                        end.
+  split. generalize e. clear e. {
   induction t.
-  - intros.
-    simpl.
-    pose proof (insert_kind_get_kind e e' v H n).
-    simpl in H0.
-    destruct (get_kind e n).
-    + unfold shift_var in H1.
-      remember (get_kind e' (if le_gt_dec v n then S n else n)) in H1 |- *.
-      destruct o.
+  - intros. simpl. apply get_free_variable. apply H.
+  - intros. simpl. destruct (highest_free_typ_variable t1); destruct (highest_free_typ_variable t2).
+    + destruct (le_gt_dec n n0).
+      * apply IHt2. apply H.
+      * apply IHt1. apply H.
+    + apply IHt1. apply H.
+    + apply IHt2. apply H.
+    + trivial.
+  - intros. simpl in H.
+    simpl. remember (highest_free_typ_variable t). destruct (highest_free_typ_variable t).
+    + rewrite Heqo in IHt |- *. destruct n.
       * trivial.
-      * inversion H1.
-    + inversion H0.
-  - intros.
-    simpl.
-    simpl in H0.
-    split.
-    { apply (IHt1 e e' v). apply H. apply H0. }
-    { apply (IHt2 e e' v). apply H. apply H0. }
-  - intros.
-    simpl. simpl in H0.
-    simpl in H0.
-    apply (IHt (etvar k e) (etvar k e') (S v)).
-    simpl.
-    split.
-    trivial.
+      * assert (S n < num_typ_variables (etvar k e) -> n < num_typ_variables e). simpl. auto with arith.
+        apply H0. apply (IHt (etvar k e) H).
+    + rewrite -> Heqo. trivial.
+  } 
+                                generalize e. clear e. induction t.
+  - intros. apply get_free_variable. apply H.
+  - intros. simpl in H. destruct (highest_free_typ_variable t1); destruct (highest_free_typ_variable t2).
+    + destruct (le_gt_dec n n0). 
+      * split. { apply IHt1. omega. } { apply IHt2. omega. }
+      * split. { apply IHt1. omega. } { apply IHt2. omega. } 
+    + split. { apply IHt1, H. } { apply IHt2. trivial. }
+    + split. { apply IHt1. trivial. } { apply IHt2, H. }
+    + split. { apply IHt1. trivial. } { apply IHt2. trivial. }
+  - intros. simpl. apply IHt. remember (highest_free_typ_variable (tall k t)) in H.
+    destruct o. simpl in Heqo.
+    destruct (highest_free_typ_variable t).
+    + destruct n0. inversion Heqo. simpl. apply lt_n_S. inversion Heqo. rewrite <- H1. apply H.
+    + trivial.
+    + simpl. simpl in Heqo. destruct (highest_free_typ_variable t). 
+      * destruct n. auto with arith. inversion Heqo.
+      * trivial.
 Qed.
 
-Theorem insert_kind_wf_env : forall (v : nat), forall (e e' : env), insert_kind v e e' -> wf_env e -> wf_env e'.
-  intros until e. generalize v. clear v.
+Lemma num_typ_variables_insert_kind : forall e e' i n,
+                                        insert_kind_r i n e e'
+                                        -> num_typ_variables e' = S (num_typ_variables e).
+  unfold insert_kind.
+  induction e.
+  - intros. destruct e'. inversion H. inversion H.
+    + destruct e'. trivial. inversion H. inversion H.
+  - intros. destruct e'. 
+    + inversion H.
+    + simpl in H. apply (IHe e' i n). apply H. 
+    + inversion H.
+  - intros. destruct e'. inversion H. inversion H.
+    destruct i.
+    + simpl in H. rewrite <- H. simpl. trivial.
+    + simpl. simpl in H. specialize (IHe e' i n).
+      assert (num_typ_variables e' = S (num_typ_variables e)). apply IHe. apply H. congruence.
+Qed.
+
+Lemma highest_free_typ_variable_tshift_s
+: forall t v, match highest_free_typ_variable (tshift (S v) t) with
+                | Some n1 =>
+                  match highest_free_typ_variable (tshift v t) with
+                    | None => n1 = 0
+                    | Some n2 => n1 <= S n2
+                  end
+                | None => True
+              end.
+  induction t.
+  - intros. unfold tshift. destruct (le_gt_dec v n); destruct (le_gt_dec (S v) n).
+    + simpl. auto with arith. + simpl. auto with arith. + simpl. trivial. + simpl. auto with arith.
+  - intros. specialize (IHt1 v). specialize (IHt2 v).
+    simpl. destruct (highest_free_typ_variable (tshift (S v) t1));
+      destruct (highest_free_typ_variable (tshift (S v) t2)).
+    + destruct (highest_free_typ_variable (tshift (v) t1));
+      destruct (highest_free_typ_variable (tshift (v) t2)).
+      destruct (le_gt_dec n n0); destruct (le_gt_dec n1 n2). 
+      omega. omega. omega. omega. destruct (le_gt_dec n n0). omega. omega.
+      destruct (le_gt_dec n n0). omega. omega. destruct (le_gt_dec n n0). omega. omega.
+    + destruct (highest_free_typ_variable (tshift v t1)).
+      destruct (highest_free_typ_variable (tshift v t2)).
+      destruct (le_gt_dec n0 n1). omega. omega. omega.
+      destruct (highest_free_typ_variable (tshift v t2)).
+      omega. omega.
+    + destruct (highest_free_typ_variable (tshift v t2)).
+      destruct (highest_free_typ_variable (tshift v t1)).
+      destruct (le_gt_dec n1 n0). omega. omega. omega.
+      destruct (highest_free_typ_variable (tshift v t1)).
+      omega. omega.
+    + trivial.
+  - intros. simpl. specialize (IHt (S v)).
+    destruct (highest_free_typ_variable (tshift (S v) t)).
+    + destruct (highest_free_typ_variable (tshift (S (S v)) t)).
+      * destruct n.
+        { destruct n0. trivial. omega. }
+        { destruct n0. trivial. omega. }
+      * trivial.
+    + destruct (highest_free_typ_variable (tshift (S (S v)) t)).
+      * destruct n. trivial. inversion IHt.
+      * trivial.
+Qed.
+
+Lemma highest_free_variable_tshift
+: forall t v, match highest_free_typ_variable t with
+                | None =>
+                  match highest_free_typ_variable (tshift v t) with
+                    | None => True
+                    | Some 0 => True
+                    | _ => False
+                  end
+                | Some t1 =>
+                    match highest_free_typ_variable (tshift v t) with
+                      | Some t2 => t2 <= S t1
+                      | None => True
+                    end
+              end.
+  induction t.
+  - intros. unfold tshift. destruct (le_gt_dec v n).
+    + simpl. trivial.
+    + simpl. auto with arith.
+  - intros. specialize (IHt1 v). specialize (IHt2 v).
+    simpl. destruct (highest_free_typ_variable t1); destruct (highest_free_typ_variable t2).
+    + destruct (le_gt_dec n n0).
+      * destruct (highest_free_typ_variable (tshift v t1));
+        destruct (highest_free_typ_variable (tshift v t2)).
+        destruct (le_gt_dec n1 n2).
+        { omega. } { omega. } { omega. } { omega. } { trivial. }
+      * destruct (highest_free_typ_variable (tshift v t1));
+        destruct (highest_free_typ_variable (tshift v t2)).
+        destruct (le_gt_dec n1 n2).
+        { omega. } { omega. } { omega. } { omega. } { trivial. }
+    + destruct (highest_free_typ_variable (tshift v t1));
+      destruct (highest_free_typ_variable (tshift v t2)).
+      destruct (le_gt_dec n0 n1). destruct n1. omega. omega. omega. omega.
+      destruct n0; [trivial | auto]. omega. omega. trivial.
+    + destruct (highest_free_typ_variable (tshift v t1));
+      destruct (highest_free_typ_variable (tshift v t2)).
+      destruct (le_gt_dec n0 n1). destruct n1. omega. omega.
+      destruct n0. omega. omega. destruct n0. omega. omega. omega. trivial.
+    + destruct (highest_free_typ_variable (tshift v t1));
+      destruct (highest_free_typ_variable (tshift v t2)).
+      destruct (le_gt_dec n n0). apply IHt2. apply IHt1. apply IHt1. apply IHt2. trivial.
+  - intros. simpl. pose proof (highest_free_typ_variable_tshift_s t v).
+    destruct (highest_free_typ_variable t). 
+    + specialize (IHt (S v)). destruct n.
+      * destruct (highest_free_typ_variable (tshift (S v) t)).
+        destruct n. trivial. destruct n. trivial. omega. trivial.
+      * destruct (highest_free_typ_variable (tshift (S v) t)).
+        destruct n0. trivial. destruct (highest_free_typ_variable (tshift v t)). omega. omega. trivial.
+    + specialize (IHt (S v)). destruct (highest_free_typ_variable (tshift (S v) t)). destruct n.
+      * trivial. * inversion IHt. * trivial.
+Qed.
+                                   
+Lemma insert_kind_wf_typ : forall (t : typ), forall (e e' : env), forall (i v : nat),
+                             insert_kind_r i v e e' ->
+                             wf_typ e t -> wf_typ e' (tshift v t).
+  intros.
+  pose proof (highest_free_variable_tshift t v).
+  pose proof (num_typ_variables_insert_kind e e' i v H).
+  pose proof (num_typ_variables_significant (tshift v t) e').
+  pose proof (num_typ_variables_significant t e).
+  rewrite H2 in H3. destruct H3. apply H5.
+  apply num_typ_variables_significant in H5.
+  - destruct (highest_free_typ_variable (tshift v t)).
+    + destruct (highest_free_typ_variable t).
+      * omega. * omega.
+    + trivial.
+  - destruct (highest_free_typ_variable (tshift v t)).
+    + destruct (highest_free_typ_variable t).
+      apply H4 in H0. omega. destruct n. omega. inversion H1.
+    + trivial.
+Qed.
+
+Theorem insert_kind_wf_env_r :
+  forall (i v : nat), forall (e e' : env), insert_kind_r i v e e' -> wf_env e -> wf_env e'.
+  intros until e. generalize i, v. clear i. clear v.
   induction e.
   - intros. destruct e'.
     + inversion H.
@@ -205,87 +395,50 @@ Theorem insert_kind_wf_env : forall (v : nat), forall (e e' : env), insert_kind 
         simpl in H0.
         destruct H.
         rewrite <- H.
-        apply (insert_kind_wf_typ t e).
-        apply H1.
+        apply (insert_kind_wf_typ t e e' i v H1).
         apply H0.
-      * apply (IHe v). apply H. apply H0.
+      * apply (IHe i v). apply H. apply H0.
     + inversion H.
   - intros. destruct e'.
     + simpl. trivial.
     + inversion H.
-    + destruct v.
+    + destruct i.
       * simpl in H. rewrite <- H. simpl. apply H0.
-      * simpl in H. simpl. apply (IHe v e'). apply H. apply H0.
+      * simpl in H. simpl. apply (IHe i v e'). apply H. apply H0.
 Qed.
-  
-Theorem insert_kind_kinding : forall (v : nat), forall (e e' : env), forall (t : typ), forall (k : kind),
-                                insert_kind v e e' -> kinding e t k -> kinding e' (tshift v t) k.
-  intros until t. generalize v, e, e'. clear v. clear e. clear e'.
+
+Theorem insert_kind_wf_env : forall v, forall e e', insert_kind v e e' -> wf_env e -> wf_env e'.
+  intros.
+  apply (insert_kind_wf_env_r v v e e'); [auto..].
+Qed.
+
+Theorem insert_kind_kinding_r : forall (i v : nat), forall (e e' : env), forall (t : typ), forall (k : kind),
+                                  insert_kind_r i v e e' -> kinding e t k -> kinding e' (tshift i t) k.
+  intros until t. generalize i, v, e, e'. clear i. clear v. clear e. clear e'.
   induction t.
   - intros.
     simpl. simpl in H0.
-    Check insert_kind_get_kind.
-    assert ((if le_gt_dec v n then S n else n) = shift_var n v).
-    auto.
+    assert ((if le_gt_dec i n then S n else n) = shift_var n i). auto.
     rewrite -> H1.
-    rewrite <- (insert_kind_get_kind e e' v H).
-    remember (get_kind e n) as k1.
-    destruct k1.
-    + split. apply (insert_kind_wf_env v e e'). apply H. apply H0. apply H0.
-    + apply H0.
+    pose proof (insert_kind_get_kind e e' i v H n). 
+    destruct (get_kind e n).
+    + rewrite -> H2.
+      split.
+      * apply (insert_kind_wf_env_r i v e e'). auto. apply H0.
+      * apply H0.
+    + inversion H0.
   - intros. simpl.
-    simpl in H0.
     do 2 destruct H0.
     exists x. exists x0.
     split.
     + apply H0.
     + split.
-      * apply (IHt1 v e). apply H. apply H0.
-      * apply (IHt2 v e). apply H. apply H0.
+      * apply (IHt1 i v e). apply H. apply H0.
+      * apply (IHt2 i v e). apply H. apply H0.
   - intros. simpl.
-    simpl in H0.
     destruct H0.
     exists x.
     split.
-    + simpl. apply (IHt (S v) (etvar k e)). simpl. auto. apply H0.
+    + apply (IHt (S i) v (etvar k e)). simpl. split. trivial. apply H. apply H0.
     + apply H0.
 Qed.
-
-Lemma insert_kind_get_typ : forall (e e' : env), forall (v : nat),
-                              insert_kind v e e' ->
-                              forall (w : nat),
-                                match get_typ e w with
-                                  | Some t => get_typ e' w = Some (tshift v t)
-                                  | None => True
-                                end.
-  induction e.
-  - intros.
-    simpl.
-    trivial.
-  - intros.
-    destruct e'.
-    + inversion H.
-    + simpl in H.
-      destruct w.
-        * simpl. symmetry. destruct H. congruence.
-        * simpl. apply IHe. apply H.
-    + inversion H.
-  - intros.
-    destruct e'.
-    + inversion H.
-    + inversion H.
-    + destruct v.
-      * simpl in H.
-        rewrite <- H.
-        remember (get_typ (etvar k e) w).
-        destruct o.
-        destruct w.
-        simpl.
-        simpl in Heqo.
-
-Theorem insert_kind_typing : forall (v : nat), forall (e e' : env), forall (t : term), forall (ty : typ),
-                               insert_kind v e e' -> typing e t ty -> typing e' t (tshift v ty).
-  intros until t. generalize v, e, e'. clear v. clear e. clear e'.
-  induction t.
-  - intros.
-    
